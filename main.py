1b9d1ac1a5b69785fc6c3d676f49867a1cfad481
andre williamson <AndreWilliamsonMN@gmail.com>
2020-12-10 23:48:35 +0000

    did a pizza order program , multiple if statements in session

--- main.py
+++ main.py
@@ -6,8 +6,24 @@ extra_cheese = input("Do you want extra cheese? Y or N ")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
+bill =0
+if size == "S":
+  print(" one small")
+  bill += 15
+elif size == " M ":
+  print("thats a mediun")
+  bill += 20
+else:
+  print(" that's a large")
+  bill += 25
 
+if add_pepperoni == "Y":
+  bill += 3
+else:
+  bill +=0
 
+if extra_cheese == "Y":
+  print(" ohhh extra for extra fatty")
+  bill += 1
 
-
-
+print(f" your bill is {bill} ")   
